c2cf3a35d8f05c39323ec3d1bf13214a3c9a6b1f
Florian Robert <florian.robert@ekino.com>
2024-06-17 10:55:07 +0200

    fix cron

--- cron.ts
+++
@@ -1,8 +0,0 @@
-import { DB } from './services/db/index.ts'
-
-Deno.cron('count slide', '*/1 * * * *', async () => {
-	const db = await DB.getInstance()
-	const iter = await db.list({ prefix: ['slides'] })
-
-	console.log(`${iter.length} slides in db`)
-})
--- main.ts
+++ main.ts
@@ -10,4 +10,14 @@ import { start } from '$fresh/server.ts'
 import manifest from './fresh.gen.ts'
 import config from './fresh.config.ts'
 
+import { DB } from './services/db/index.ts'
+
+Deno.cron('count slide', '*/1 * * * *', async () => {
+    const db = await DB.getInstance()
+    const iter = await db.list({ prefix: ['slides'] })
+
+    console.log(`${iter.length} slides in db`)
+})
+
+
 await start(manifest, config)
